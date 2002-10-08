394180ec5fa0ac80f8a616681d9fc6b1e089b9fb
vektor <vektor>
2002-10-08 17:57:33 +0000

    Some small doc updates.

--- mixer.h
+++ mixer.h
@@ -1,6 +1,7 @@
-
 /**
  * Copyright (C) 2002 Doug Bell <drbell@users.sourceforge.net>
+ *
+ * Mixer routines stolen from mplayer, http://mplayer.sourceforge.net.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
