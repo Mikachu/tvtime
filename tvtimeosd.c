a58254d93454ba1029c0c6c5994ec27f0b1f2bd2
vektor <vektor>
2002-10-17 02:32:58 +0000

    Working on the OSD API...

--- tvtimeosd.c
+++ tvtimeosd.c
@@ -1,4 +1,5 @@
 
+#include <stdlib.h>
 #include "osd.h"
 #include "tvtimeosd.h"
 
@@ -7,6 +8,7 @@ struct tvtime_osd_s
     osd_string_t *channel_number;
     osd_string_t *volume_bar;
     osd_string_t *muted_osd;
+    int muted;
 };
 
 tvtime_osd_t *tvtime_osd_new( int width, int height, double frameaspect )
@@ -16,11 +18,57 @@ tvtime_osd_t *tvtime_osd_new( int width, int height, double frameaspect )
         return 0;
     }
 
+    osd->channel_number = osd_string_new( "helr.ttf", 80, width,
+                                          height, frameaspect );
+    osd_string_set_colour( osd->channel_number, 220, 12, 155 );
+    osd_string_show_border( osd->channel_number, 1 );
+
+    osd->volume_bar = osd_string_new( "helr.ttf", 15, width,
+                                      height, frameaspect );
+    osd_string_set_colour( osd->volume_bar, 200, 128, 128 );
+
+    osd->muted_osd = osd_string_new( "helr.ttf", 15, width,
+                                     height, frameaspect );
+    osd_string_set_colour( osd->muted_osd, 200, 128, 128 );
+    osd_string_show_text( osd->muted_osd, "Mute", 100 );
+    osd->muted = 0;
+
     return osd;
 }
 
 void tvtime_osd_delete( tvtime_osd_t *osd )
 {
     free( osd );
+}
+
+void tvtime_osd_show_channel_number( tvtime_osd_t *osd, const char *text )
+{
+    osd_string_show_text( osd->channel_number, text, 80 );
+}
+
+void tvtime_osd_show_bar( tvtime_osd_t *osd, const char *barname,
+                          int percentage )
+{
+    char bar[ 108 ];
+    memset( bar, 0, 108 );
+    strcpy( bar, barname );
+    memset( bar + 7, '|', percentage );
+    osd_string_show_text( osd->volume_bar, bar, 80 );
+}
+
+void tvtime_osd_volume_muted( tvtime_osd_t *osd, int mutestate )
+{
+    osd->muted = mutestate;
+}
+
+void tvtime_osd_composite_packed422( tvtime_osd_t *osd, unsigned char *output,
+                                     int width, int height, int stride )
+{
+}
+
+void tvtime_osd_advance_frame( tvtime_osd_t *osd )
+{
+    osd_string_advance_frame( osd->channel_number );
+    osd_string_advance_frame( osd->volume_bar );
 }
 
--- tvtimeosd.h
+++ tvtimeosd.h
@@ -8,5 +8,13 @@ typedef struct tvtime_osd_s tvtime_osd_t;
 tvtime_osd_t *tvtime_osd_new( int width, int height, double frameaspect );
 void tvtime_osd_delete( tvtime_osd_t *osd );
 
+void tvtime_osd_show_channel_number( tvtime_osd_t *osd, const char *text );
+void tvtime_osd_show_bar( tvtime_osd_t *osd, const char *barname,
+                          int percentage );
+void tvtime_osd_volume_muted( tvtime_osd_t *osd, int mutestate );
+void tvtime_osd_composite_packed422( tvtime_osd_t *osd, unsigned char *output,
+                                     int width, int height, int stride );
+void tvtime_osd_advance_frame( tvtime_osd_t *osd );
+
 
 #endif /* TVTIMEOSD_H_INCLUDED */
