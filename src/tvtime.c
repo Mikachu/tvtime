45817e5e9d6eccbc13a8a324dbde60140aee5d6c
drbell <drbell>
2002-11-16 18:23:30 +0000

    moved console down to bottom/middle

--- src/tvtime.c
+++ src/tvtime.c
@@ -634,7 +634,7 @@ int main( int argc, char **argv )
     }
 
     /* Setup the console */
-    con = console_new( ct, 10,10, (width*80)/100, (height*20)/100, 10,
+    con = console_new( ct, (width*10)/100, height - (height*20)/100, (width*80)/100, (height*20)/100, 10,
                        width, height, 
                        config_get_aspect( ct ) ? (16.0 / 9.0) : (4.0 / 3.0) );
     if( !con ) {
