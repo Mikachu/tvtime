9050c01c42d9de638218ba469d74709f71251615
vektor <vektor>
2002-11-17 18:24:27 +0000

    MMX'ified the linear blend deinterlacer plugin.

--- plugins/linearblend.c
+++ plugins/linearblend.c
@@ -37,10 +37,58 @@ static void deinterlace_scanline_linear_blend( unsigned char *output,
                                         unsigned char *t0, unsigned char *m0,
                                         unsigned char *b0, int width )
 {
+    int i;
+
+    READ_PREFETCH_2048( t1 );
+    READ_PREFETCH_2048( b1 );
+    READ_PREFETCH_2048( m1 );
+
+    // Get width in bytes.
     width *= 2;
+    i = width / 8;
+    width -= i * 8;
+
+    pxor_r2r( mm7, mm7 );
+    while( i-- ) {
+        movd_m2r( *t1, mm0 );
+        movd_m2r( *b1, mm1 );
+        movd_m2r( *m1, mm2 );
+
+        movd_m2r( *(t1+4), mm3 );
+        movd_m2r( *(b1+4), mm4 );
+        movd_m2r( *(m1+4), mm5 );
+
+        punpcklbw_r2r( mm7, mm0 );
+        punpcklbw_r2r( mm7, mm1 );
+        punpcklbw_r2r( mm7, mm2 );
+
+        punpcklbw_r2r( mm7, mm3 );
+        punpcklbw_r2r( mm7, mm4 );
+        punpcklbw_r2r( mm7, mm5 );
+
+        psllw_i2r( 1, mm2 );
+        psllw_i2r( 1, mm5 );
+        paddw_r2r( mm0, mm2 );
+        paddw_r2r( mm3, mm5 );
+        paddw_r2r( mm1, mm2 );
+        paddw_r2r( mm4, mm5 );
+        psrlw_i2r( 2, mm2 );
+        psrlw_i2r( 2, mm5 );
+        packuswb_r2r( mm2, mm2 );
+        packuswb_r2r( mm5, mm5 );
+
+        movd_r2m( mm2, *output );
+        movd_r2m( mm5, *(output+4) );
+        output += 8;
+        t1 += 8;
+        b1 += 8;
+        m1 += 8;
+    }
     while( width-- ) {
         *output++ = (*t1++ + *b1++ + (2 * *m1++))>>2;
     }
+    sfence();
+    emms();
 }
 
 static void deinterlace_scanline_linear_blend2( unsigned char *output, unsigned char *m2,
@@ -48,10 +96,58 @@ static void deinterlace_scanline_linear_blend2( unsigned char *output, unsigned 
                            unsigned char *b1, unsigned char *t0,
                            unsigned char *b0, int width )
 {
+    int i;
+
+    READ_PREFETCH_2048( t1 );
+    READ_PREFETCH_2048( b1 );
+    READ_PREFETCH_2048( m2 );
+
+    // Get width in bytes.
     width *= 2;
+    i = width / 8;
+    width -= i * 8;
+
+    pxor_r2r( mm7, mm7 );
+    while( i-- ) {
+        movd_m2r( *t1, mm0 );
+        movd_m2r( *b1, mm1 );
+        movd_m2r( *m2, mm2 );
+
+        movd_m2r( *(t1+4), mm3 );
+        movd_m2r( *(b1+4), mm4 );
+        movd_m2r( *(m2+4), mm5 );
+
+        punpcklbw_r2r( mm7, mm0 );
+        punpcklbw_r2r( mm7, mm1 );
+        punpcklbw_r2r( mm7, mm2 );
+
+        punpcklbw_r2r( mm7, mm3 );
+        punpcklbw_r2r( mm7, mm4 );
+        punpcklbw_r2r( mm7, mm5 );
+
+        psllw_i2r( 1, mm2 );
+        psllw_i2r( 1, mm5 );
+        paddw_r2r( mm0, mm2 );
+        paddw_r2r( mm3, mm5 );
+        paddw_r2r( mm1, mm2 );
+        paddw_r2r( mm4, mm5 );
+        psrlw_i2r( 2, mm2 );
+        psrlw_i2r( 2, mm5 );
+        packuswb_r2r( mm2, mm2 );
+        packuswb_r2r( mm5, mm5 );
+
+        movd_r2m( mm2, *output );
+        movd_r2m( mm5, *(output+4) );
+        output += 8;
+        t1 += 8;
+        b1 += 8;
+        m2 += 8;
+    }
     while( width-- ) {
         *output++ = (*t1++ + *b1++ + (2 * *m2++))>>2;
     }
+    sfence();
+    emms();
 }
 
 
@@ -61,7 +157,7 @@ static deinterlace_method_t linearblendmethod =
     "Linear Blend (idea from mplayer)",
     "LinearBlend",
     1,
-    0,
+    MM_ACCEL_X86_MMXEXT,
     0,
     0,
     deinterlace_scanline_linear_blend,
