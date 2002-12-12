8adc14d6695e79831116a395e008e530890271bf
vektor <vektor>
2002-12-12 15:33:19 +0000

    Added more copyright messages.

--- plugins/greedy.c
+++ plugins/greedy.c
@@ -1,6 +1,20 @@
-
-// Copyright (c) 2000 Tom Barry  All rights reserved.
-
+/**
+ * Copyright (c) 2000 Tom Barry  All rights reserved.
+ * mmx.h port copyright (c) 2002 Billy Biggs <vektor@dumbterm.net>.
+ *
+ * This code is ported from DScaler: http://deinterlace.sf.net/
+ *
+ * This file is subject to the terms of the GNU General Public License as
+ * published by the Free Software Foundation.  A copy of this license is
+ * included with this software distribution in the file COPYING.  If you
+ * do not have a copy, you may obtain a copy by writing to the Free
+ * Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details
+ */
 
 #include <stdio.h>
 #include <inttypes.h>
