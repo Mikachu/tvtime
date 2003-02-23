75429b86723cceb684422d585325ea1a24b5053c
asbelov <asbelov>
2003-02-23 18:28:59 +0000

    Adding xml_config_set function for replace ar add configuration item

--- src/tvtimeconf_xml.c
+++ src/tvtimeconf_xml.c
@@ -13,8 +13,28 @@
 typedef struct
 {
     char *name;
-    char *value;
+    char *val;
 } ConfigDef;
+
+/*
+ type can be:
+    c - command /int/
+    i - int
+    u - unsigned int
+    f - double
+    b - int /boolean/
+    l - long long
+    s - *char
+*/
+
+typedef struct
+{
+    char *name;
+    void *val;
+    char type;
+    Conf *next;
+} Conf;
+
 
 typedef struct
 {
@@ -115,6 +135,7 @@ static const KeyBindDef key_def[] =
     {'r',	TVTIME_SKIP_CHANNEL},
     {' ',	TVTIME_AUTO_ADJUST_PICT},
     {'=',	TVTIME_TOGGLE_HALF_FRAMERATE},
+    {"b",	TVTIME_DETECT_SCANLINE_BIAS},
     {0, 0}
 };
 
@@ -175,14 +196,172 @@ static const Cmd_Names cmd_table[] = {
     { "SKIP_CHANNEL", TVTIME_SKIP_CHANNEL },
     { "TOGGLE_CC", TVTIME_TOGGLE_CC },
     { "TOGGLE_HALF_FRAMERATE", TVTIME_TOGGLE_HALF_FRAMERATE },
+    { "SCAN_CHANNELS", TVTIME_SCAN_CHANNELS },
+    { "OVERSCAN_UP", TVTIME_OVERSCAN_UP },
+    { "OVERSCAN_DOWN", TVTIME_OVERSCAN_DOWN },
+    { "DETECT_SCANLINE_BIAS", TVTIME_DETECT_SCANLINE_BIAS },
+    { "CHANNEL_1", TVTIME_CHANNEL_1 },
+    { "CHANNEL_2", TVTIME_CHANNEL_2 },
+    { "CHANNEL_3", TVTIME_CHANNEL_3 },
+    { "CHANNEL_4", TVTIME_CHANNEL_4 },
+    { "CHANNEL_5", TVTIME_CHANNEL_5 },
+    { "CHANNEL_6", TVTIME_CHANNEL_6 },
+    { "CHANNEL_7", TVTIME_CHANNEL_7 },
+    { "CHANNEL_8", TVTIME_CHANNEL_8 },
+    { "CHANNEL_9", TVTIME_CHANNEL_9 },
+    { "CHANNEL_0", TVTIME_CHANNEL_0 }, 
     { NULL, NULL }
 };
 
 
-xmlDocPtr xml_config_init(char *docname)
+int xml_string_to_command( const char *str )
+{
+    int i;
+
+    if( !str || !*str ) return TVTIME_NOCOMMAND;
+
+    for(i = 0; cmd_table[i].name != NULL; i++)
+	if( !strcasecmp( cmd_table[i].name, str ) ) return cmd_table[i].command;
+
+    return TVTIME_NOCOMMAND;
+}
+
+Conf *xml_config_set(Conf *C_root, const char *name, const char *value, const char *type)
+{
+    Conf *C_cur, *C_prev=NULL;
+
+    if( !name || !*name ||
+	!value || !*value ||
+	!type || !*type ) return C_root;
+
+/* Search item and replase the value */
+    for(C_cur=C_root; C_cur != NULL; C_cur=C_cur->next)
+    {
+	C_prev=C_cur;
+	if(!strcasecmp(name, C_cur->name) && *type == C_cur->type)
+	{
+	    switch (*type)
+	    {
+		case 'i':
+		case 'u':
+		    memcpy(C_cur->val, atoi(value), sizeof(int));
+		    break;
+		case 'f':
+		    memcpy(C_cur->val, value, sizeof(double));
+		    break;
+		case 'c':
+		    memcpy(C_cur->val, string_to_command(value), sizeof(int));
+		    break;
+		case 'b':
+		    switch (*value)
+		    {
+			case 'T': /* True */
+			case 't': /* true */
+			case '1': /* 1    */
+			case 'Y': /* Yes  */
+			case 'y': /* yes  */
+			    memcpy(C_cur->val, (const int)1, sizeof(int));
+			    break;
+			case 'F': /* False */
+			case 'f': /* false */
+			case '0': /* 0     */
+			case 'N': /* No    */
+			case 'n': /* no    */
+			default:
+			    memcpy(C_cur->val, (const int)0, sizeof(int));
+			    break;
+		    }
+		    break;
+		case 'l':
+		    memcpy(C_cur->val, value, sizeof(long long));
+		    break;
+		case 's':
+		default:
+		    if(C_cur->val != NULL)
+		    {
+			if(!strcasecmp(value, (char *)C_cur->val)) break;
+			free(C_cur->val);
+		    }
+		    if((C_cur->val = (void *)strdup(value)) == NULL)
+			fprintf(stderr, "xml_config_merge(): Can't allocate memory");
+		    break;
+	    }
+	    return C_root;
+	}
+    }
+
+/* Item not found. Add new item */
+
+    if(C_cur = (Conf *) malloc(sizeof(Conf)) == NULL ) return NULL;
+    
+    switch (*type)
+    {
+	case 'i':
+	case 'u':
+    	    if((C_cur->val = (void *)malloc(sizeof(int))) == NULL ||
+		C_cur->name = strdup(name)) { free(C_cur); return NULL; }
+	    memcpy(C_cur->val, value, sizeof(int));
+	    break;
+
+	case 'f':
+	    if((C_cur->val = (void *)malloc(sizeof(double))) == NULL ||
+		C_cur->name = strdup(name)) { free(C_cur); return NULL; }
+	    memcpy(C_cur->val, value, sizeof(double));
+	    break;
+		
+	case 'c':
+	    if((C_cur->val = (void *)malloc(sizeof(int))) == NULL ||
+		C_cur->name = strdup(name)) { free(C_cur); return NULL; }
+	    memcpy(C_cur->val, string_to_command(value), sizeof(int));
+	    break;
+
+	case 'b':
+	    if((C_cur->val = (void *)malloc(sizeof(int))) == NULL ||
+		C_cur->name = strdup(name)) { free(C_cur); return NULL; }
+	    switch (*value)
+	    {
+		case 'T': /* True */
+		case 't': /* true */
+		case '1': /* 1    */
+		case 'Y': /* Yes  */
+		case 'y': /* yes  */
+		    memcpy(C_cur->val, (const int)1, sizeof(int));
+		    break;
+		case 'F': /* False */
+		case 'f': /* false */
+		case '0': /* 0     */
+		case 'N': /* No    */
+		case 'n': /* no    */
+		default:
+		    memcpy(C_cur->val, (const int)0, sizeof(int));
+		    break;
+	    }
+	    break;
+	case 'l':
+	    if((C_cur->val = (void *)malloc(sizeof(long long))) == NULL ||
+		C_cur->name = strdup(name)) { free(C_cur); return NULL; }
+	    memcpy(C_cur->val, value, sizeof(long long));
+	    break;
+
+	case 's':
+	default:
+	    if((C_cur->val = (void *)strdup(value)) == NULL ||
+		C_cur->name = strdup(name)) { free(C_cur); return NULL; }
+	    break;
+    }
+    
+    if(C_root != NULL) C_prev->next=C_cur;
+    else C_root=C_cur;
+    C_cur->next=NULL;
+    return C_root;
+}
+
+Conf *xml_config_init(char *docname)
 {
     xmlDocPtr doc;
     xmlNodePtr cur;
+    Conf *C_root, *C_cur, *C_prev;
+    char *type, *value;
     
     if((doc = xmlParseFile(docname)) == NULL)
     {
@@ -204,7 +383,31 @@ xmlDocPtr xml_config_init(char *docname)
 	return NULL;
     }
 
-    return doc;
+    
+    cur = cur->xmlChildrenNode;
+    while (cur != NULL)
+    {
+	if ((!xmlStrcmp(cur->name, (const xmlChar *)"Global")))
+	{
+	    cur = cur->xmlChildrenNode;
+	    break;
+	}
+    }
+    
+    for (C_root=NULL; cur != NULL; cur=cur->next)
+	if ((!xmlStrcmp(cur->name, (const xmlChar *)"entry")))
+	    xml_config_set(C_root, xmlGetProp(cur, "name"), xmlGetProp(cur, "value"), mlGetProp(cur, "type")))
+
+
+    xmlFreeDoc(doc);
+    return C_root;
+}
+
+Conf *xml_config_new(int argc, char **argv)
+{
+    Conf *C;
+    xmlDocPtr doc;
+    
 }
 
 #endif /* #ifdef HAVE_LIBXML2 */
