edee67bc8adab747bb21223f8a3ffab2527d187a
vektor <vektor>
2003-04-20 18:15:44 +0000

    I hate making new files, but I think this will be useful.

---
+++ src/utils.h
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2003 Billy Biggs <vektor@dumbterm.net>.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or (at
+ * your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#ifndef UTILS_H_INCLUDED
+#define UTILS_H_INCLUDED
+
+/**
+ * Useful functions that don't belong anywhere else.
+ */
+int file_is_openable_for_read( const char *filename );
+
+#endif /* UTILS_H_INCLUDED */
