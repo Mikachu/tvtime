1b0f41e2f774cf45a02d39a5da8822e0be5ddfa3
vektor <vektor>
2003-05-12 19:50:40 +0000

    * tvtime/src/pngoutput.h: Updated copyright year, added documentation.   * tvtime/src/pngoutput.c: Updated copyright year.

--- src/pngoutput.c
+++ src/pngoutput.c
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2002 Billy Biggs <vektor@dumbterm.net>.
+ * Copyright (C) 2002, 2003 Billy Biggs <vektor@dumbterm.net>.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- src/pngoutput.h
+++ src/pngoutput.h
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2002 Billy Biggs <vektor@dumbterm.net>.
+ * Copyright (C) 2002, 2003 Billy Biggs <vektor@dumbterm.net>.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,11 +25,26 @@
 extern "C" {
 #endif
 
+/**
+ * Example usage:
+ *
+ * pngoutput_t *pngout = pngoutput_new( "myimage.png", width, height, 1, 0.45, 1 );
+ *
+ * for( i = 0; i < height; i++ ) {
+ *     pngoutput_scanline( pngout, myimage + (i*bytesperline) );
+ * }
+ *
+ * pngoutput_delete( pngout );
+ */
+
 typedef struct pngoutput_s pngoutput_t;
 
 /**
- * Creates a new PNG output file at the given width and height.  The
- * gamma should be the intended
+ * Creates a new PNG output file at the given width and height.  If
+ * alpha is true, then the input is 32bit-per-pixel RGBA.  If
+ * compression is true, the png file will be compressed.  The gamma
+ * value is the inverse of the display gamma, so, for a display gamma
+ * of 2.2, use a gamma value of 0.45.
  */
 pngoutput_t *pngoutput_new( const char *filename, int width, int height,
                             int alpha, double gamma, int compression );
@@ -40,7 +55,8 @@ pngoutput_t *pngoutput_new( const char *filename, int width, int height,
 void pngoutput_delete( pngoutput_t *pngoutput );
 
 /**
- * Writes a scanline (of the appropriate width) to the PNG file.
+ * Writes a scanline (of the appropriate width) to the PNG file.  Scanlines
+ * should be provided from top to bottom of the image.
  */
 void pngoutput_scanline( pngoutput_t *pngoutput, uint8_t *scanline );
 
