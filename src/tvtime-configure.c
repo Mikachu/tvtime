e47f743d21e9f0535cb3d4555909e90ccf48fcf2
vektor <vektor>
2003-08-07 00:42:51 +0000

    06 Aug 2003  Billy Biggs <vektor@dumbterm.net>
      * tvtime/src/tvtime-configure.c: Added a new app to reconfigure the
          tvtime configuration file.
      * tvtime/src/Makefile.am: Add tvtime-configure.
      * tvtime/src/.cvsignore: Ignore the binary file in CVS.
      * tvtime/docs/tvtime-configure.1: Added a dummy man page, totally wrong
          for now. :)
      * tvtime/ChangeLog: Note this new app in the changelog.

---
+++ src/tvtime-configure.c
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2003 Billy Biggs <vektor@dumbterm.net>.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "tvtimeconf.h"
+
+int main( int argc, char **argv )
+{
+    config_t *cfg = config_new();
+    if( !cfg ) {
+        fprintf( stderr, "tvtime-configure: Can't initialize tvtime configuration, exiting.\n" );
+        return 1;
+    }
+
+    config_parse_tvtime_command_line( cfg, argc, argv );
+
+    config_delete( cfg );
+    return 0;
+}
+
