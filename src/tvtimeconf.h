afbdd52879b753dc25b12568712468cd6de25dd3
vektor <vektor>
2003-09-18 17:03:02 +0000

    18 Sep 2003  Billy Biggs <vektor@dumbterm.net>
      * tvtime/src/tvtimeconf.h: Better document the tvtime config file API.

--- src/tvtimeconf.h
+++ src/tvtimeconf.h
@@ -27,26 +27,122 @@
 extern "C" {
 #endif
 
+/**
+ * The config object represents the data contained in the tvtime config
+ * file and specified on the command line, and also provides an API for
+ * writing information back to a config file.
+ *
+ * However, these are separate: the read data is immutable, and so
+ * values written using this API may not be later queried by the
+ * application.
+ *
+ * This object handles parsing the command line and therefore also
+ * prints the usage message for tvtime, tvtime-command, tvtime-scanner
+ * and tvtime-configure.
+ */
+
 typedef struct config_s config_t;
 
+/**
+ * Create a new config object.  This will initialize the internal data
+ * structures and attempt to read the default config file from the
+ * config file in $sysconfdir/tvtime/tvtime.xml and then override these
+ * with the values in $HOME/.tvtime/tvtime.xml.
+ */
 config_t *config_new( void );
+
+/**
+ * Deletes the config file object, freeing all internal data and closing
+ * the config file writer.
+ */
 void config_delete( config_t *ct );
+
+/**
+ * This is the API to the config file saving.  This writes the option
+ * with the given name and new value to the config file in $HOME, or if
+ * the --configfile parameter is used, to that file instead.
+ */
+void config_save( config_t *ct, const char *name, const char *value );
+
+/**
+ * Parses the command line for the 'tvtime' command.
+ */
 int config_parse_tvtime_command_line( config_t *ct, int argc, char **argv );
+
+/**
+ * Parses the command line for the 'tvtime-configure' command.
+ */
 int config_parse_tvtime_config_command_line( config_t *ct, int argc, char **argv );
+
+/**
+ * Parses the command line for the 'tvtime-scanner' command.
+ */
 int config_parse_tvtime_scanner_command_line( config_t *ct, int argc, char **argv );
 
+/**
+ * Returns the tvtime command corresponding to the given key event.
+ */
 int config_key_to_command( config_t *ct, int key );
+
+/**
+ * Returns the argument to give the tvtime command corresponding to the
+ * given key event.
+ */
 const char *config_key_to_command_argument( config_t *ct, int key );
+
+/**
+ * Given a tvtime command, this returns the first bound key event listed
+ * in the config file.
+ */
 int config_command_to_key( config_t *ct, int command );
 
+/**
+ * Returns the tvtime menu command corresponding to a key.  This is for
+ * mapping keys when we're in menu mode, so that keys can be doubled-up
+ * between menu and normal mode (for example, the arrow keys).
+ */
 int config_key_to_menu_command( config_t *ct, int key );
+
+/**
+ * Returns the argument to give the tvtime menu command corresponding to
+ * the given key event.
+ */
 const char *config_key_to_menu_command_argument( config_t *ct, int key );
+
+/**
+ * Given a tvtime menu command, this returns the first bound key event
+ * listed in the config file.
+ */
 int config_menu_command_to_key( config_t *ct, int command );
 
+/**
+ * Given a mouse button, this returns the tvtime command bound to that
+ * button press event.
+ */
 int config_button_to_command( config_t *ct, int button );
+
+/**
+ * Given a mouse button, this returns the argument to give the tvtime
+ * command bound to that button press event.
+ */
 const char *config_button_to_command_argument( config_t *ct, int button );
+
+/**
+ * Given a mouse button, this returns the tvtime menu command bound to
+ * that button press event.
+ */
 int config_button_to_menu_command( config_t *ct, int button );
+
+/**
+ * Given a mouse button, this returns the argument to give the tvtime
+ * menu command bound to that button press event.
+ */
 const char *config_button_to_menu_command_argument( config_t *ct, int button );
+
+/**
+ * The following represents all of the information stored in a tvtime
+ * config file.
+ */
 
 int config_get_verbose( config_t *ct );
 int config_get_send_fields( config_t *ct );
@@ -87,18 +183,13 @@ int config_get_global_colour( config_t *ct );
 int config_get_global_hue( config_t *ct );
 const char *config_get_audio_mode( config_t *ct );
 const char *config_get_xmltv_file( config_t *ct );
-
 double config_get_overscan( config_t *ct );
-
 int config_get_check_freq_present( config_t *ct );
-
 int config_get_usexds( config_t *ct );
 char *config_get_vbidev( config_t *ct );
 
 int config_get_num_modes( config_t *ct );
 config_t *config_get_mode_info( config_t *ct, int mode );
-
-void config_save( config_t *ct, const char *name, const char *value );
 
 #ifdef __cplusplus
 };
