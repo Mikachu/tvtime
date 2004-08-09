a6ab55731aa5f0386182498c7e5010d6f58777e7
vektor <vektor>
2004-08-09 01:41:42 +0000

    08 Aug 2004  Billy Biggs <vektor@dumbterm.net>
      * tvtime/plugins/tomsmocomp/SearchLoop0A.inc: Adding Dirk Ziegelmeier's
         port of the TomsMoComp deinterlacer.
      * tvtime/plugins/tomsmocomp/SearchLoopBottom.inc: Ibid.
      * tvtime/plugins/tomsmocomp/SearchLoopEdgeA.inc: Ibid.
      * tvtime/plugins/tomsmocomp/SearchLoopEdgeA8.inc: Ibid.
      * tvtime/plugins/tomsmocomp/SearchLoopOddA.inc: Ibid.
      * tvtime/plugins/tomsmocomp/SearchLoopOddA2.inc: Ibid.
      * tvtime/plugins/tomsmocomp/SearchLoopOddA6.inc: Ibid.
      * tvtime/plugins/tomsmocomp/SearchLoopOddAH.inc: Ibid.
      * tvtime/plugins/tomsmocomp/SearchLoopOddAH2.inc: Ibid.
      * tvtime/plugins/tomsmocomp/SearchLoopTop.inc: Ibid.
      * tvtime/plugins/tomsmocomp/SearchLoopVA.inc: Ibid.
      * tvtime/plugins/tomsmocomp/SearchLoopVAH.inc: Ibid.
      * tvtime/plugins/tomsmocomp/StrangeBob.inc: Ibid.
      * tvtime/plugins/tomsmocomp/TomsMoCompAll.inc: Ibid.
      * tvtime/plugins/tomsmocomp/TomsMoCompAll2.inc: Ibid.
      * tvtime/plugins/tomsmocomp/WeirdBob.inc: Ibid.
      * tvtime/plugins/tomsmocomp/tomsmocompfilter_3dnow.cpp: Ibid.
      * tvtime/plugins/tomsmocomp/tomsmocompfilter_mmx.cpp: Ibid.
      * tvtime/plugins/tomsmocomp/tomsmocompfilter_sse.cpp: Ibid.
      * tvtime/plugins/tomsmocomp/tomsmocompmacros.h: Ibid.

---
+++ plugins/tomsmocomp/tomsmocompfilter_mmx.cpp
@@ -0,0 +1,27 @@
+/***************************************************************************
+                           tomsmocompfilter_mmx.cpp
+                           ------------------------
+    begin                : Sat Jul 10 2004
+    copyright            : (C) 2004 by Dirk Ziegelmeier
+    email                : dziegel@gmx.de
+***************************************************************************/
+
+/*
+ * TomsMoComp algorithm taken from DScaler.
+ * Copyright (c) 2002 Tom Barry.  All rights reserved.
+ *
+ * Ported by: Dirk Ziegelmeier
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ */
+
+#define IS_MMX
+#define SSE_TYPE MMX
+#define FUNCT_NAME filterDScaler_MMX
+#include "TomsMoCompAll.inc"
+#undef  IS_MMX
+#undef  SSE_TYPE
+#undef  FUNCT_NAME
