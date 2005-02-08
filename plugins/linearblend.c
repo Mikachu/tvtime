e7e24e0d48cea6defc62a80627e8419865a45d3d
vektor <vektor>
2005-02-08 05:08:17 +0000

    * tvtime/plugins/*.c: Relicense deinterlacer code under the LGPL.   * tvtime/COPYING.LGPL: Add a copy of the LGPL.   * tvtime/ChangeLog: Note this in the ChangeLog.

--- plugins/linearblend.c
+++ plugins/linearblend.c
@@ -1,24 +1,22 @@
 /**
- * Linear blend deinterlacing plugin.  The algorithm for this filter is based
- * on the mythtv sources, which took it from the mplayer sources.
+ * Linear blend deinterlacing plugin.  The idea for this algorithm came
+ * from the linear blend deinterlacer which originated in the mplayer
+ * sources.
  *
- * The file is postprocess_template.c in mplayer, and is
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- *  Copyright (C) 2001-2002 Michael Niedermayer (michaelni@gmx.at)
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the GNU
+ * Lesser General Public License for more details.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
  */
 
 #include <stdio.h>
